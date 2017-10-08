543fe5d2cbed9c404e141df7e7d5b6f855d6799b
MicheledG <chiefzephyr@gmail.com>
2017-10-08 17:15:33 +0200

    Completed results printer

--- src/main/java/it/polito/mdg/lorawan/simulator/util/ResultsWriter.java
+++ src/main/java/it/polito/mdg/lorawan/simulator/util/ResultsWriter.java
@@ -2,7 +2,10 @@ package it.polito.mdg.lorawan.simulator.util;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
 import java.util.Collections;
+import java.util.Date;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
@@ -11,20 +14,32 @@ import it.polito.mdg.lorawan.simulator.model.SimulationResult;
 
 public class ResultsWriter {
 
-	private final static String DEFAULT_RESULTS_FILE_NAME ="results.json";
+	private final static String DEFAULT_RESULTS_FILE_FOLDER ="results";
+	private final static String DEFAULT_RESULTS_FILE_NAME_PREFIX ="results";
+	private final static String DEFAULT_RESULTS_FILE_NAME_SUFFIX =".json";	
+	private final static DateFormat DEFAULT_TIMESTAMP_FORMAT = new SimpleDateFormat("yyyyMMdd-HHmmss");
 	
 	public static void writeResults(SimulationResult results, String resultsFileName) throws IOException{
 		
 		//sort the results
 		Collections.sort(results.getResults());
 		
+		//create the results path
+		String resultsPath = DEFAULT_RESULTS_FILE_FOLDER + "/";
+		
 		//print to the file
 		if(resultsFileName == null){
-			resultsFileName = DEFAULT_RESULTS_FILE_NAME;
+			resultsPath += DEFAULT_RESULTS_FILE_NAME_PREFIX
+					+ "-"
+					+ DEFAULT_TIMESTAMP_FORMAT.format(new Date())
+					+ DEFAULT_RESULTS_FILE_NAME_SUFFIX;
+		}
+		else{
+			resultsPath += resultsFileName;
 		}
 		
 		//create the file writer where to push the results
-		FileWriter jsonFileWriter = new FileWriter(resultsFileName);	
+		FileWriter jsonFileWriter = new FileWriter(resultsPath);	
 		//create ObjectWriter to serialize the results object into the json file
 		ObjectWriter objectWriter = new ObjectMapper().writerWithDefaultPrettyPrinter();				
 		objectWriter.writeValue(jsonFileWriter , results);		
